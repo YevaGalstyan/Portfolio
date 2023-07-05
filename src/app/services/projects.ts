9975fc7bf1d64f8139de701adbcf593cd4afa156
YevaGalstyan01 <eva.galstyan@insource.am>
2023-07-05 21:08:43 +0400

    minor changes in individual projects

--- src/app/services/projects.ts
+++ src/app/services/projects.ts
@@ -17,13 +17,13 @@ export class ProjectsService {
             name: this.localize.localize.medControl + ' / МЕД УПРАВЛЕНИЕ',
             date: '2022 ' + this.localize.localize.nov + ' - ' + this.localize.localize.present,
             description: this.localize.localize.medControlService,
-            imgUrl: 'med.png',
+            imgUrls: ['med.png', 'med3.png', 'med1.png', 'med2.png'],
             link: 'https://app.med-upravlenie.ru/',
 
             // Outer - card information
-            cardHeader: this.localize.localize.imperialCapital,
-            outerImgUrl: '/projects/trade_small.png',
-            cardInfo: this.localize.localize.tradingServiceShort
+            cardHeader: this.localize.localize.medControl,
+            outerImgUrl: '/projects/med_small.png',
+            cardInfo: this.localize.localize.medControlServiceShort
         },
 
         {
@@ -32,13 +32,13 @@ export class ProjectsService {
             name: this.localize.localize.imperialCapital,
             date: '2021 ' + this.localize.localize.may + ' - ' + '2021 ' + this.localize.localize.sep,
             description: this.localize.localize.tradingService,
-            imgUrl: 'trade.png',
+            imgUrls: ['trade.png'],
             link: 'https://imperialcg.com/',
 
             // Outer - card information
-            cardHeader: this.localize.localize.medControl,
-            outerImgUrl: '/projects/med_small.png',
-            cardInfo: this.localize.localize.medControlServiceShort
+            cardHeader: this.localize.localize.imperialCapital,
+            outerImgUrl: '/projects/trade_small.png',
+            cardInfo: this.localize.localize.tradingServiceShort
         }
     ]
 }
@@ -53,7 +53,7 @@ export interface project {
     name: string
     date: string
     link: string
-    imgUrl: string
+    imgUrls: string[]
     description: string
 
     // Outer - card information
